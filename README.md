de088f791b72584fd4178c506487c4e1f40f4fee
Cartoon Zhang <jtzhang@i9i8.com>
2016-09-02 20:19:51 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+Hi, I'm Cartoon Daemon @palmtale, here's my resume page http://m.glad.so
+You can contact me via m@glad.so
---
+++ index.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+var path = require('path'),
+    log4js = require('log4js'),
+    KOA = require('koa'),
+    koahbs  = require('koa-handlebars'),
+
+//Init app
+var koa = KOA();
+
+//Config app
+log4js.configure(app.config.log4js, { cwd: app.config.log4js.cwd });
+
+koa.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
+koa.use(routers);
+koa.use(Express.static(path.join(__dirname, 'public')));
+
+koa.engine('.hbs', exphbs({defaultLayout: "default", extname: ".hbs"}));
+koa.set('view engine', '.hbs');
+
+//Start app
+koa.listen(process.env.PORT || 5000);
